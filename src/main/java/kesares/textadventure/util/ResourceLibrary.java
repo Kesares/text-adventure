a8a257e8b0a8b72a31f878f0bd61fe9bcb6f2bc0
Kesares <kesares19@gmail.com>
2023-09-08 01:33:25 +0200

    Change resource bundle name

--- src/main/java/kesares/textadventure/util/ResourceLibrary.java
+++ src/main/java/kesares/textadventure/util/ResourceLibrary.java
@@ -7,9 +7,9 @@ import java.util.Properties;
 public class ResourceLibrary {
 
     private static final ResourceLibrary INSTANCE = new ResourceLibrary(new ResourceLoader());
-    private static final String ROOT = "root";
-    private static final String ROOT_EN = "root_en";
-    private static final String ROOT_DE = "root_de";
+    private static final String LANG = "lang";
+    private static final String LANG_EN = "lang_en";
+    private static final String LANG_DE = "lang_de";
 
     private final ResourceLoader resourceLoader;
     private final Map<String, Properties> langResources;
@@ -25,12 +25,12 @@ public class ResourceLibrary {
     }
 
     private void loadResources() {
-        this.langResources.put(ROOT, this.resourceLoader.loadPropertiesFrom("/lang/" + ROOT + ".properties"));
-        this.langResources.put(ROOT_EN, this.resourceLoader.loadPropertiesFrom("/lang/" + ROOT_EN + ".properties"));
-        this.langResources.put(ROOT_DE, this.resourceLoader.loadPropertiesFrom("/lang/" + ROOT_DE + ".properties"));
+        this.langResources.put(LANG, this.resourceLoader.loadPropertiesFrom("/lang/" + LANG + ".properties"));
+        this.langResources.put(LANG_EN, this.resourceLoader.loadPropertiesFrom("/lang/" + LANG_EN + ".properties"));
+        this.langResources.put(LANG_DE, this.resourceLoader.loadPropertiesFrom("/lang/" + LANG_DE + ".properties"));
     }
 
     public synchronized Properties getLangResource(String key) {
-        return this.langResources.getOrDefault(key, this.langResources.get(ROOT));
+        return this.langResources.getOrDefault(key, this.langResources.get(LANG));
     }
 }
