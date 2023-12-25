f34c30089a00060fd68a8551bd2a564f091e4e33
Kesares <kesares19@gmail.com>
2023-12-25 19:32:05 +0100

    Change local selection to language selection

--- src/main/java/kesares/textadventure/core/Settings.java
+++ src/main/java/kesares/textadventure/core/Settings.java
@@ -3,6 +3,7 @@ package kesares.textadventure.core;
 import kesares.textadventure.io.InputManager;
 import kesares.textadventure.io.MenuPrinter;
 import kesares.textadventure.io.OutputManager;
+import kesares.textadventure.util.lang.Language;
 import kesares.textadventure.util.lang.LanguageSelector;
 
 import java.util.Locale;
@@ -33,8 +34,8 @@ public final class Settings {
         OutputManager.clearConsole();
         byte option = MenuPrinter.printLanguageSelectionMenu();
         switch (option) {
-            case 1 -> LANGUAGE_SELECTOR.changeLanguageTo(Locale.ENGLISH);
-            case 2 -> LANGUAGE_SELECTOR.changeLanguageTo(Locale.GERMAN);
+            case 1 -> LANGUAGE_SELECTOR.changeLanguageTo(Language.ENGLISH);
+            case 2 -> LANGUAGE_SELECTOR.changeLanguageTo(Language.GERMAN);
             case 3 -> {}
             default -> {
                 OutputManager.printOptionDoesntExist(option);
