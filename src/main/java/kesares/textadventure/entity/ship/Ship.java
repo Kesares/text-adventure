0da62e746abf11d66eca52bfacf490c0a82567f5
Kesares <kesares19@gmail.com>
2024-07-13 03:48:24 +0200

    Refactor Ship.java and add cannon arrays to ships

--- src/main/java/kesares/textadventure/entity/ship/Ship.java
+++ src/main/java/kesares/textadventure/entity/ship/Ship.java
@@ -1,35 +1,31 @@
 package kesares.textadventure.entity.ship;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import kesares.textadventure.item.Cannon;
+import kesares.textadventure.item.Items;
+
+import java.util.Arrays;
 
 @JsonDeserialize(builder = Ship.Settings.class)
 public class Ship {
 
     private final int hp;
-    private final int cannons;
-    private final int harpoons;
     private final int goldCosts;
     private final int diamondCosts;
+    private final Cannon[] cannons;
+    private final int harpoons;
 
-    public Ship(Settings settings) {
+    private Ship(Settings settings) {
         this.hp = settings.hp;
-        this.cannons = settings.cannons;
-        this.harpoons = settings.harpoons;
         this.goldCosts = settings.goldCosts;
         this.diamondCosts = settings.diamondCosts;
+        this.cannons = settings.cannons;
+        this.harpoons = settings.harpoons;
     }
 
     public int getHp() {
         return hp;
-    }
-
-    public int getCannons() {
-        return cannons;
-    }
-
-    public int getHarpoons() {
-        return harpoons;
     }
 
     public int getGoldCosts() {
@@ -40,55 +36,60 @@ public class Ship {
         return diamondCosts;
     }
 
+    public Cannon[] getCannons() {
+        return cannons;
+    }
+
+    public int getHarpoons() {
+        return harpoons;
+    }
+
+    @JsonPOJOBuilder(withPrefix = "")
     public static class Settings {
 
         private int hp = 2_500;
-        private int cannons = 10;
-        private int harpoons = 10;
         private int goldCosts = 0;
         private int diamondCosts = 0;
+        private Cannon[] cannons = new Cannon[10];
+        private int harpoons = 10;
 
         public static Ship copyOf(Ship ship) {
-            return new Settings().hp(ship.hp).cannons(ship.cannons).harpoons(ship.harpoons).goldCosts(ship.goldCosts)
-                    .diamondCosts(ship.diamondCosts).build();
+            return new Settings().hp(ship.hp).goldCosts(ship.goldCosts).diamondCosts(ship.diamondCosts)
+                    .cannons(ship.cannons).harpoons(ship.harpoons).build();
         }
 
-        @JsonProperty("hp")
         public Settings hp(int hp) {
             if (hp < 1) throw new IllegalArgumentException("hp < 1");
             this.hp = hp;
             return this;
         }
 
-        @JsonProperty("cannons")
-        public Settings cannons(int cannons) {
-            if (cannons < 0) throw new IllegalArgumentException("cannons < 0");
-            this.cannons = cannons;
-            return this;
-        }
-
-        @JsonProperty("harpoons")
-        public Settings harpoons(int harpoons) {
-            if (harpoons < 0) throw new IllegalArgumentException("harpoons < 0");
-            this.harpoons = harpoons;
-            return this;
-        }
-
-        @JsonProperty("goldCosts")
         public Settings goldCosts(int goldCosts) {
             if (goldCosts < 0) throw new IllegalArgumentException("goldCosts < 0");
             this.goldCosts = goldCosts;
             return this;
         }
 
-        @JsonProperty("diamondCosts")
         public Settings diamondCosts(int diamondCosts) {
             if (diamondCosts < 0) throw new IllegalArgumentException("diamondCosts < 0");
             this.diamondCosts = diamondCosts;
             return this;
         }
 
+        public Settings cannons(Cannon[] cannons) {
+            if (cannons == null) throw new NullPointerException();
+            this.cannons = cannons;
+            return this;
+        }
+
+        public Settings harpoons(int harpoons) {
+            if (harpoons < 0) throw new IllegalArgumentException("harpoons < 0");
+            this.harpoons = harpoons;
+            return this;
+        }
+
         public Ship build() {
+            Arrays.fill(this.cannons, Items.SIX_POUNDER);
             return new Ship(this);
         }
     }
