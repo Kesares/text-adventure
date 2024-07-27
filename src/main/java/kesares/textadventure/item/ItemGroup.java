d74c326c88e20cd0934096ae7805e17781e44b9d
Kesares <kesares19@gmail.com>
2024-07-27 17:31:56 +0200

    Remove harpoons item group

--- src/main/java/kesares/textadventure/item/ItemGroup.java
+++ src/main/java/kesares/textadventure/item/ItemGroup.java
@@ -3,7 +3,6 @@ package kesares.textadventure.item;
 public enum ItemGroup {
 
     CANNONS("Cannons"),
-    HARPOONS("Harpoons"),
     CANNONBALLS("Cannonballs"),
     MISCELLANEOUS("Miscellaneous");
 
